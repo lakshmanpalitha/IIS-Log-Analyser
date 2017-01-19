21a503302b89f5c5c96a1ac4e3a8469ebf770c6d
lakshman <lpa@codehousegroup.com>
2017-01-19 17:10:39 +0530

    Merge branch 'master' into develop
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
-(function($) {
+(function($) {  
   
-  
-	$( document ).ready(function() {
-	  
+	$(document).ready(function() {
+	    $('.query-editor').simpleIntellisense();
+
+	    $('.run-query').click(function () {
+	        var queryEditor = $(this).parents('.query-editor-holder').find('.query-editor'),
+                query = queryEditor[0].innerText;
+
+	        console.log(query);
+//	        $.ajax({
+//	            url: 'demo_test.txt',
+//	            success: function (result) {
+//	                $('#').html(result);
+//	            }
+//	        });
+	    });
 	});
   
 })(jQuery);
