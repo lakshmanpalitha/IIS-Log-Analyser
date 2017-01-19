f30e504b389eb7618c3aea528d3168068a18c963
Dhanushka Jayathilake <dja@codehousegroup.com>
2017-01-19 12:38:52 +0530

    plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,7 @@
-(function($) {
+(function($) {  
   
-  
-	$( document ).ready(function() {
-	  
+	$(document).ready(function() {
+	  $('.query-editor').simpleIntellisense();
 	});
   
 })(jQuery);
--- js/simple-intellisense.js
+++ js/simple-intellisense.js
@@ -1,16 +1,33 @@
 (function ($) {
-	$.fn.simple-intellisense = function(options ) {
+	$.fn.simpleIntellisense = function(options) {
 		
 		var settings = $.extend({
-			'hints': ['select', 'p_id', 'p_name', 'p_description']
+			'hints': ['select', 'p_id', 'p_name', 'p_description'],
+			'ignoreKeyCodes': [13]
 		}, options);
 
 		return this.each(function() {
-			var $this = $(this);
+			var $element = $(this);
 			
-			$this.keyup( function( e ) {
-				var character = string.fromCharCode(e.which);
+			$element.keypress( function(e) {
+				var key = String.fromCharCode(e.which);
+				var hints = getHints($element, key);
+				if (hints.length > 0) {
+					showHints(hints);
+				}
 			});
+		});
+		
+		function getHints($element, key) {
+			var text = $element.text();
+			$.grep(settings.hints, function(item, i) {
+					
+			});
+			return settings.hints;
+		}
+		
+		function showHints(hints) {
+			alert(hints);
 		}
 	};
 })(jQuery);
