48fcfb3c5d062157ed616363e6dd54baca6aafa2
lakshman <lpa@codehousegroup.com>
2017-01-19 17:07:54 +0530

    changes ajex

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 (function($) {  
   
 	$(document).ready(function() {
-	  $('.query-editor').simpleIntellisense();
+	    $('.query-editor').simpleIntellisense();
+
+	    $('.run-query').click(function () {
+	        var queryEditor = $(this).parents('.query-editor-holder').find('.query-editor'),
+                query = queryEditor[0].innerText;
+
+	        console.log(query);
+//	        $.ajax({
+//	            url: 'demo_test.txt',
+//	            success: function (result) {
+//	                $('#').html(result);
+//	            }
+//	        });
+	    });
 	});
   
 })(jQuery);
