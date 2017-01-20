eee4eb9a13f7a914d9964066a023ceaabdff90fe
ajshehan <sas@codehousegroup.com>
2017-01-20 10:21:35 +0530

    intigrate front end styling

---
+++ IISLogAnalyzer/Scripts/scripts.js
@@ -0,0 +1,39 @@
+(function($) {  
+  
+	$(document).ready(function() {
+	    $('.query-editor').simpleIntellisense();
+
+	    $('.run-query').click(function () {
+	        var queryEditor = $(this).parents('.query-editor-holder').find('.query-editor'),
+                query = queryEditor[0].innerText;
+
+	        console.log(query);
+//	        $.ajax({
+//	            url: 'demo_test.txt',
+//	            success: function (result) {
+//	                $('#').html(result);
+//	            }
+//	        });
+	    });
+
+        //TO BE EDITED
+	    $("#results").html('');
+
+	    $("#btnQuery").click(function () {
+
+	        var path = $("#txtLogFilePath").val();
+	        var query = $("#txtQuery").val();
+
+	        $.ajax({
+	            type: 'POST',
+	            url: "Index",
+	            data: { path: path, query: query },
+	            success: function (data) {
+	                $("#results").html(data);
+	            }
+	        });
+	    });
+	});
+
+  
+})(jQuery);
