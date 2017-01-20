98a5e36da63134b8b8913146f64e5cdf11dda6cd
pasan <psa@codehousegroup.com>
2017-01-20 11:44:11 +0530

    dynamic path and query

--- IISLogAnalyzer/Scripts/scripts.js
+++ IISLogAnalyzer/Scripts/scripts.js
@@ -1,39 +1,40 @@
-(function($) {  
-  
-	$(document).ready(function() {
-	    $('.query-editor').simpleIntellisense();
+(function ($) {
 
-	    $('.run-query').click(function () {
-	        var queryEditor = $(this).parents('.query-editor-holder').find('.query-editor'),
+    $(document).ready(function () {
+        $('.query-editor').simpleIntellisense();
+
+        $('.run-query').click(function () {
+            var queryEditor = $(this).parents('.query-editor-holder').find('.query-editor'),
                 query = queryEditor[0].innerText;
 
-	        console.log(query);
-//	        $.ajax({
-//	            url: 'demo_test.txt',
-//	            success: function (result) {
-//	                $('#').html(result);
-//	            }
-//	        });
-	    });
+            console.log(query);
+            //	        $.ajax({
+            //	            url: 'demo_test.txt',
+            //	            success: function (result) {
+            //	                $('#').html(result);
+            //	            }
+            //	        });
+        });
 
         //TO BE EDITED
-	    $("#results").html('');
+        $("#results").html('');
 
-	    $("#btnQuery").click(function () {
+        $("#btnQuery").click(function () {
 
-	        var path = $("#txtLogFilePath").val();
-	        var query = $("#txtQuery").val();
+            var path = $("#txtLogFilePath").val();
+            var query = $("#txtQuery").val();
+            var logType = $("#dropLogType").val();
 
-	        $.ajax({
-	            type: 'POST',
-	            url: "Index",
-	            data: { path: path, query: query },
-	            success: function (data) {
-	                $("#results").html(data);
-	            }
-	        });
-	    });
-	});
+            $.ajax({
+                type: 'POST',
+                url: "Index",
+                data: { path: path, query: query, logType: logType },
+                success: function (data) {
+                    $("#results").html(data);
+                }
+            });
+        });
+    });
 
-  
+
 })(jQuery);
